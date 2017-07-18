858a38d8c1d40f774850d6d4f87a700abfa1d8ac
imagentsmith <iamagentsmith@yandex.ru>
2017-07-18 14:50:16 +0300

    Fix transfer email service(v2)

--- app/services/transfer_email_service.rb
+++ app/services/transfer_email_service.rb
@@ -2,7 +2,7 @@ module TransferEmailService
   class << self
     def call
       Company.find_each do |company|
-        unless company.emails.first.empty?
+        if company.emails.first
           ActiveRecord::Base.transaction do
             company.email = company.emails.first.value
             company.sended = company.emails.first.sended
