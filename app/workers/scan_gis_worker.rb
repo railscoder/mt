9801cfebc9490321b8ca07560dac9d2ef7ac059f
imagentsmith <iamagentsmith@yandex.ru>
2017-07-28 00:40:07 +0300

    Fix scan gis worker

--- app/workers/scan_gis_worker.rb
+++ app/workers/scan_gis_worker.rb
@@ -1,7 +1,7 @@
 class ScanGisWorker
   include Sidekiq::Worker
 
-  def perform(query, city_id, source)
+  def perform(query, city_id)
     ScanGisService.call(query, city_id, source)
   end
   # Sidekiq::Queue.all.each(&:clear)
