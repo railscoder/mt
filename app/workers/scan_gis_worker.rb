2343da9503c6a9578ac980c5c3a2848f8d52723d
imagentsmith <iamagentsmith@yandex.ru>
2017-07-28 00:57:06 +0300

    Delete source from gis worker

--- app/workers/scan_gis_worker.rb
+++ app/workers/scan_gis_worker.rb
@@ -2,7 +2,7 @@ class ScanGisWorker
   include Sidekiq::Worker
 
   def perform(query, city_id)
-    ScanGisService.call(query, city_id, source)
+    ScanGisService.call(query, city_id)
   end
   # Sidekiq::Queue.all.each(&:clear)
 end
