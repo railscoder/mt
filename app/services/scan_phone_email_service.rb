9f4bead4010f0dcb715f1b21161fe1de1b29c116
imagentsmith <iamagentsmith@yandex.ru>
2017-08-03 22:52:58 +0300

    Fix ScanPhoneEmailService

--- app/services/scan_phone_email_service.rb
+++ app/services/scan_phone_email_service.rb
@@ -12,6 +12,11 @@ module ScanPhoneEmailService
 
     def find_email_on_site(company)
       begin
+        full_url = "http://" + company.site
+        page = open_page(full_url)
+        link_to_contact = get_contact_page(page, full_url)
+        contact_page = open_page(link_to_contact)
+        email = get_email(page) || get_email(contact_page)
         ActiveRecord::Base.transaction do
           company.email = email
           company.save
