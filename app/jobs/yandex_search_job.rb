2ec9ac1394008cae93cfe17b380756e29ab98ef9
imagentsmith <iamagentsmith@yandex.ru>
2017-08-08 03:55:57 +0300

    Add queries(v2)

--- app/jobs/yandex_search_job.rb
+++ app/jobs/yandex_search_job.rb
@@ -4,7 +4,7 @@ class YandexSearchJob < ApplicationJob
   def perform(city_id, client)
     File.open("queries.txt", "r").each_line do |query|
       YandexSearchService.call(query.chomp, city_id, client)
-      sleep(30)
+      sleep(60)
     end
   end
 end
