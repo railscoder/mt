8c607f3f3f190053cd9598476912e81f01d98a26
imagentsmith <iamagentsmith@yandex.ru>
2017-08-09 01:06:06 +0300

    Change params to client_id in ScanPhoneEmailService

--- app/services/scan_phone_email_service.rb
+++ app/services/scan_phone_email_service.rb
@@ -2,8 +2,8 @@ module ScanPhoneEmailService
   class << self
     include ScanEmail
 
-    def call(source, category_id)
-      Company.where(email: nil, source: source, category_id: category_id).find_each do |company|
+    def call(client_id)
+      Company.where(client_id).find_each do |company|
         find_email_on_site(company)
       end
     end
