9fff5743c7c8aca6ecc2379aa59cc82830daa4ad
imagentsmith <iamagentsmith@yandex.ru>
2017-08-15 01:57:50 +0300

    Fix scan sites worker

--- app/jobs/yandex_search_job.rb
+++ app/jobs/yandex_search_job.rb
@@ -1,10 +1,10 @@
 class YandexSearchJob < ApplicationJob
   queue_as :default
 
-  def perform(city_id, client)
-    File.open("queries.txt", "r").each_line do |query|
-      YandexSearchService.call(query.chomp, city_id, client)
-      sleep(60)
-    end
+  def perform(query, city_id, client)
+    #File.open("queries.txt", "r").each_line do |query|
+      YandexSearchService.call(query, city_id, client)
+      #sleep(60)
+    #end
   end
 end
